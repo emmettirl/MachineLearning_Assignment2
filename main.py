678072e288794aea5fc117d4a268bce7531bf2b0
Emmett <emmettirl@gmail.com>
2024-11-15 22:40:54 +0000

    Blocked out project with temp sklearn models, task 2 complete

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from copyreg import constructor
 
+from sklearn.linear_model import LogisticRegression, Perceptron
+from sklearn.model_selection import KFold
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
 source_filename = 'fashion-mnist_train'
 file_extension = '.csv'
 main_cache_filename = source_filename + ".pkl"
@@ -19,12 +25,13 @@ TARGET_LABELS = {
     9: 'Ankle_boot'
 }
 
+num_samples=1000
+
 ########################################################################################################################
 # Task 1:  pre-processing and visualisation
 ########################################################################################################################
 
 def task1(df):
-    print_header('*', 'Task 1: Pre-processing and Visualisation')
 
     target_df = df[df.iloc[:, 0].isin(TARGET_LABELS.keys())]
 
@@ -35,9 +42,10 @@ def task1(df):
     for label in unique_labels:
         first_instance = features[labels == label].iloc[0]
         display_image(label, first_instance)
+
+    print('Unique labels:', unique_labels)
+    print('Number of instances:', len(features))
     return labels, features
-
-
 
 def separate_labels_and_features(df):
     labels = df.iloc[:, 0]  # First column as labels
@@ -54,51 +62,155 @@ def display_image(label, features):
     plt.imshow(pixels, cmap='gray')
     plt.show()
 
+########################################################################################################################
+# Task 2:  Evaluation Procedure
+########################################################################################################################
+
+def task2(labels, features, classifier, num_samples=None):
+
+    if num_samples:
+        features = features.sample(n=num_samples, random_state=42)
+        labels = labels.loc[features.index]
+
+    kf = KFold(n_splits=5, shuffle=True, random_state=42)
+    results = []
+
+    i = 0
+    for train_index, test_index in kf.split(features):
+
+        start_time = time.time()
+        X_train, X_test = features.iloc[train_index], features.iloc[test_index]
+        y_train, y_test = labels.iloc[train_index], labels.iloc[test_index]
+
+        classifier.fit(X_train, y_train)
+        training_time = time.time() - start_time
+
+        y_pred = classifier.predict(X_test)
+        prdiction_time = time.time() - start_time - training_time
+
+        accuracy = (y_pred == y_test).mean()
+        results.append((accuracy, training_time, prdiction_time))
+
+        print(f'Fold {i + 1}: Accuracy: {accuracy:.2f}, Training Time: {training_time:.2f}s, Prediction Time: {prdiction_time:.2f}s')
+        i += 1
+
+    # print confusion matrix
+    OutputFormat.print_divider('h2')
+    print('Confusion Matrix\n')
+    print(pd.crosstab(y_test, y_pred, rownames=['Actual'], colnames=['Predicted']))
+
+    # print average results
+    OutputFormat.print_divider('h2')
+
+    results_df = pd.DataFrame(results, columns=['Accuracy', 'Training Time', 'Prediction Time'])
+
+    summary_df = pd.DataFrame({
+        'Min': results_df.min(),
+        'Max': results_df.max(),
+        'Average': results_df.mean()
+    })
+
+    print('Summary Results\n')
+    print(summary_df)
+
+    return results_df
+
+
+########################################################################################################################
+# Task 3:  Perceptron Classifier
+########################################################################################################################
+
+def task3(labels, features, num_samples=None):
+    classifier = Perceptron()
+    results_df = task2(labels, features, classifier, num_samples)
+
+########################################################################################################################
+# Task 4:  Decision Tree Classifier
+########################################################################################################################
+
+def task4(labels, features, num_samples=None):
+    classifier = DecisionTreeClassifier()
+    results_df = task2(labels, features, classifier, num_samples)
+
+########################################################################################################################
+# Task 5:  K-Nearest Neighbours Classifier
+########################################################################################################################
+
+def task5(labels, features, num_samples=None, n_neighbors = 5):
+    classifier = KNeighborsClassifier(n_neighbors)
+    results_df = task2(labels, features, classifier, num_samples)
+
+
+########################################################################################################################
+# Task 6:  Support Vector Machine Classifier
+########################################################################################################################
+
+def task6(labels, features, num_samples=None):
+    classifier = SVC()
+    results_df = task2(labels, features, classifier, num_samples)
+
+########################################################################################################################
+# Task 7:  Classifier Comparison
+########################################################################################################################
+
+def task7():
+    pass
 
 ########################################################################################################################
 # Output Formatting
 ########################################################################################################################
-def print_header(char, text):
-    print('\n' + char * 80)
-    print(text.center(80, ' '))
-    print(str(char * 80) + '\n')
+class OutputFormat:
+    SECTION_DIVIDER = {
+        'title': '▀',
+        'h1': '#',
+        'h2': '*',
+        'h3': '-'
+    }
 
+    def __init__(self):
+        pass
 
-def print_divider(char):
-    print("\n" + char * 80 + "\n")
+    @staticmethod
+    def print_header(section, text):
+        print('\n' + OutputFormat.SECTION_DIVIDER[section] * 80)
+        print(text.center(80, ' '))
+        print(str(OutputFormat.SECTION_DIVIDER[section] * 80) + '\n')
 
+    @staticmethod
+    def print_divider(section):
+        print("\n" + OutputFormat.SECTION_DIVIDER[section] * 80 + "\n")
 
-# Function to display a progress bar so the user knows the program is still running and how far along it is
-def progressbar(i, upper_range, start_time):
-    # Calculate the percentage of completion
-    percentage = (i / (upper_range - 1)) * 100
-    # Calculate the number of '█' characters to display
-    num_blocks = int(percentage/2)
-    # Calculate elapsed time and estimated remaining time
+    @staticmethod
+    # Function to display a progress bar so the user knows the program is still running and how far along it is
+    def progressbar(i, upper_range, start_time):
+        # Calculate the percentage of completion
+        percentage = (i / (upper_range - 1)) * 100
+        # Calculate the number of '█' characters to display
+        num_blocks = int(percentage/2)
+        # Calculate elapsed time and estimated remaining time
 
-    elapsed_time = time.time() - start_time
-    if percentage > 0:
-        estimated_total_time = elapsed_time / (percentage / 100)
-        remaining_time = estimated_total_time - elapsed_time
-    else:
-        remaining_time = 0
+        elapsed_time = time.time() - start_time
+        if percentage > 0:
+            estimated_total_time = elapsed_time / (percentage / 100)
+            remaining_time = estimated_total_time - elapsed_time
+        else:
+            remaining_time = 0
 
-    # Create the progress bar string
-    progress_string = f'\r{("█" * num_blocks)}{("_" * (50 - num_blocks))} {percentage:.2f}% | Elapsed: {elapsed_time:.2f}s | Remaining: {remaining_time:.2f}s'
-    if i == upper_range - 1:
-        print(progress_string)
-    else:
-        print(progress_string, end='', flush=True)
+        # Create the progress bar string
+        progress_string = f'\r{("█" * num_blocks)}{("_" * (50 - num_blocks))} {percentage:.2f}% | Elapsed: {elapsed_time:.2f}s | Remaining: {remaining_time:.2f}s'
+        if i == upper_range - 1:
+            print(progress_string)
+        else:
+            print(progress_string, end='', flush=True)
 
 
 ########################################################################################################################
 # Main
 ########################################################################################################################
 
-
-
 def main():
-    print_header('*', 'Machine Learning Project')
+
+    OutputFormat.print_header('title', 'Machine Learning Assignment 2')
 
     start_time = time.time()  # start measuring time
 
@@ -109,7 +221,29 @@ def main():
     original_data = read_data()
     df = original_data.copy()
 
+    OutputFormat.print_header('h1', 'Task 1: Pre-processing and Visualisation')
     labels, features = task1(df)
+
+
+    OutputFormat.print_header('h1', 'Task 2: Evaluation Procedure')
+    test_classifier = LogisticRegression(max_iter=1000)
+    task2(labels, features, test_classifier, num_samples)
+
+    OutputFormat.print_header('h1', 'Task 3: Perceptron Classifier')
+    task3(labels, features, num_samples)
+
+    OutputFormat.print_header('h1', 'Task 4: Decision Tree Classifier')
+    task4(labels, features, num_samples)
+
+    OutputFormat.print_header('h1', 'Task 5: K-Nearest Neighbours Classifier')
+    task5(labels, features, num_samples, 5)
+
+    OutputFormat.print_header('h1', 'Task 6: Support Vector Machine Classifier')
+    task6(labels, features, num_samples)
+
+    OutputFormat.print_header('h1', 'Task 7: Classifier Comparison')
+    task7()
+
 
 
 
